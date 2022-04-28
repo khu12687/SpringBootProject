9c97572082b132bde0ad9d474609ae5d91a8aca6
hywooKim <khu12687>
2022-04-28 15:02:24 +0900

    spring boot 1일차

---
+++ src/main/java/kr/ac/kopo/hello/RootController.java
@@ -0,0 +1,16 @@
+package kr.ac.kopo.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RootController {
+	@GetMapping("/")
+	public String index(Model model) {
+		
+		model.addAttribute("msg", "안녕하세요");
+		
+		return "index";
+	}
+}
---
+++ src/test/java/kr/ac/kopo/hello/HelloApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
